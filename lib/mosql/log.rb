712ca9025a89ec88f9444c69cb7349ee5c99b58c
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-02-06 20:11:48 +0100

    Add outputter to a log

--- lib/mosql/log.rb
+++ lib/mosql/log.rb
@@ -1,13 +1,13 @@
 module MoSQL
   module Logging
     def log
-      @@logger ||= generate_logger
+      @@logger ||=
     end
 
-    def generate_logger
+    def generate_log
       log = Log4r::Logger.new("Stripe::MoSQL")
-      log << Log4r::Outputter.stdout
-      log << Log4r::FileOutputter.new('logs', :filename =>  'logs.log')
+      log.outputters << Log4r::Outputter.stdout
+      log.outputters << Log4r::FileOutputter.new('logs', :filename =>  'logs.log')
       log
     end
   end
