bcbe6aa67c196e83addaf96481a11d1fdd39ec03
Shreyas Agarwal <shreyas.agarwal@gmail.com>
2017-02-06 19:53:28 +0100

    Add logger at once

--- lib/mosql/log.rb
+++ lib/mosql/log.rb
@@ -1,10 +1,14 @@
 module MoSQL
   module Logging
     def log
-      @@logger ||= Log4r::Logger.new("Stripe::MoSQL")
-      @@logger.outputters << Log4r::Outputter.stdout
-      @@logger.outputters << Log4r::FileOutputter.new('logs', :filename =>  'logs.log')
-      @@logger
+      @@logger ||= generate_logger
+    end
+
+    def generate_logger
+      log = Log4r::Logger.new("Stripe::MoSQL")
+      log << Log4r::Outputter.stdout
+      log << Log4r::FileOutputter.new('logs', :filename =>  'logs.log')
+      log
     end
   end
 end
